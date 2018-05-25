78e9b5400b6be7dd270672723ea424d8af6bb9aa
VeritasSoftware <shantanu_negi@hotmail.com>
2018-05-25 19:31:57 +1000

    fixed up unit tests.

--- src/app/components/login/login.component.spec.ts
+++ src/app/components/login/login.component.spec.ts
@@ -1,21 +1,35 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { NgbTooltipConfig} from '@ng-bootstrap/ng-bootstrap';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
 
 import { LoginComponent } from './login.component';
+import { ValidationService } from '../../services/validation-service';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+  let ngbTooltipConfig: NgbTooltipConfig;
+  let validationService: ValidationService;  
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      declarations: [ LoginComponent ],
+      imports: [FormsModule, NgbModule.forRoot(),],
+      providers: [NgbTooltipConfig, ValidationService]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
+    ngbTooltipConfig = TestBed.get(NgbTooltipConfig);
+    validationService = TestBed.get(ValidationService);    
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+  });  
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
   });  
 });
--- src/app/components/register/register.component.spec.ts
+++ src/app/components/register/register.component.spec.ts
@@ -1,25 +1,35 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {NgbTooltipConfig} from '@ng-bootstrap/ng-bootstrap';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
 
 import { RegisterComponent } from './register.component';
+import { ValidationService } from '../../services/validation-service';
 
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
   let fixture: ComponentFixture<RegisterComponent>;
+  let ngbTooltipConfig: NgbTooltipConfig;
+  let validationService: ValidationService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ RegisterComponent ]
+      declarations: [ RegisterComponent ],
+      imports: [FormsModule, NgbModule.forRoot(),],
+      providers: [NgbTooltipConfig, ValidationService]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
+    ngbTooltipConfig = TestBed.get(NgbTooltipConfig);
+    validationService = TestBed.get(ValidationService);
     fixture = TestBed.createComponent(RegisterComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  // it('should create', () => {
-  //   expect(component).toBeTruthy();
-  // });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
 });
